a77d5fd06e9aa648f705b54b914728db2a335a43
Marcel Hebing <marcel@ddionrails.org>
2016-08-11 16:48:41 +0200

    Add all fields to instruments.json #5 #6

--- lib_py/merge_instruments.py
+++ lib_py/merge_instruments.py
@@ -1,5 +1,6 @@
 import json
 import yaml
+from collections import defaultdict
 import pandas as pd
 
 def import_tables():
@@ -10,12 +11,23 @@ def import_tables():
     )
     return tables
 
-def gen_instruments(tables):
+def get_answers(tables):
+    answers = defaultdict(list)
+    for i, answer in tables["answers"].iterrows():
+        answer = dict(answer)
+        key = (answer["questionnaire"], answer["answer_list"])
+        answers[key].append(answer)
+    return answers
+
+def get_instruments(tables):
     instrument_list = [dict(row) for i, row in tables["questionnaires"].iterrows()]
     instruments = dict([(x["questionnaire"], x) for x in instrument_list])
     for instrument in instruments.values():
         instrument["instrument"] = instrument["questionnaire"]
         instrument["questions"] = list()
+    return instruments
+
+def fill_questions(tables, instruments, answers):
     for i, question in tables["questions"].iterrows():
         question = dict(question)
         i_name = question["questionnaire"]
@@ -24,12 +36,15 @@ def gen_instruments(tables):
                 instrument=i_name,
                 questions=list()
             )
+        try:
+            key = (question["questionnaire"], question["answer_list"])
+            question["answers"] = answers[key]
+        except:
+            question["answers"] = ""
         instruments[i_name]["questions"].append(question)
     return instruments
 
-def main(export_json=True, export_yaml=False):
-    tables = import_tables()
-    instruments = gen_instruments(tables)
+def export(instruments, export_json=True, export_yaml=False):
     if export_json:
         with open("ddionrails/instruments.json", "w") as f:
             json.dump(instruments, f)
@@ -37,5 +52,12 @@ def main(export_json=True, export_yaml=False):
         with open("ddionrails/instruments.yaml", "w") as f:
             yaml.dump(instruments, f, default_flow_style=False)
 
+def main(export_json=True, export_yaml=False):
+    tables = import_tables()
+    answers = get_answers(tables)
+    instruments = get_instruments(tables)
+    fill_questions(tables, instruments, answers)
+    export(instruments, export_json, export_yaml)
+
 if __name__ == "__main__":
-    main()
+    main(export_yaml=True)
