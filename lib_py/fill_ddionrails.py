389663aa56942b8c4f56dc81ef16d687d92f5720
mhebing <marcel@ddionrails.org>
2016-09-20 11:38:30 +0200

    Create datasets.json

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 sys.path.append(os.path.expanduser("~/github/ddi.py/"))
 
-from ddi.onrails.repos import merge_instruments, dor1, copy
+from ddi.onrails.repos import merge_instruments, dor1, copy, convert_r2ddi
 
 def main():
     copy.study()
@@ -11,6 +11,7 @@ def main():
     dor1.variables()
     dor1.questions_variables()
     merge_instruments.main()
+    convert_r2ddi.Parser(version="v2013").write_json()
     copy.r2ddi("r2ddi/v2013/en", "ddionrails/r2ddi/v2013")
 
 if __name__ == "__main__":
