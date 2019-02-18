cfcc0a506ca5e28900351362b83d44ad984fc87c
afuetterer <fuetterh@posteo.de>
2019-02-18 16:13:59 +0100

    Refactor fill_ddionrails.py

--- lib_py/fill_ddionrails.py
+++ lib_py/fill_ddionrails.py
@@ -1,15 +1,18 @@
 from ddi.onrails.repos import convert_r2ddi, copy, dor1, merge_instruments
 from questions_variables import questions_from_generations
 
+STUDY = "soep-is"
+VERSION = "v2015.1"
+
 
 def main():
     copy.study()
     dor1.datasets()
     dor1.variables()
     dor1.concepts_questions()
-    questions_from_generations()
+    questions_from_generations(VERSION)
     merge_instruments.main()
-    convert_r2ddi.Parser("soep-is", version="v2015.1").write_json()
+    convert_r2ddi.Parser(STUDY, version=VERSION).write_json()
     copy.bibtex()
 
 
