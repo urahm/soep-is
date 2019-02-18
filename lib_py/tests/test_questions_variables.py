97483fe74bc643f87e0f657686f527c59234044f
afuetterer <fuetterh@posteo.de>
2019-02-18 16:09:30 +0100

    Update tests

--- lib_py/tests/test_questions_variables.py
+++ lib_py/tests/test_questions_variables.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from pandas.util.testing import assert_frame_equal
-from questions_variables import (create_indirect_links_once,
-                                 create_indirect_links_recursive,
-                                 questions_from_generations)
+from questions_variables import (
+    create_indirect_links_once,
+    create_indirect_links_recursive,
+    create_questions_from_generations,
+)
 
 
 def test_create_indirect_links_once():
@@ -20,8 +22,18 @@ def test_create_indirect_links_recursive():
 
 
 def test_questions_from_generations():
-    result = questions_from_generations(
-        "tests/data/logical_variables.csv", "tests/data/generations_run_once.csv"
+    result = create_questions_from_generations(
+        "y1", "tests/data/logical_variables.csv", "tests/data/generations_run_once.csv"
+    )
+    expected = pd.read_csv("tests/data/questions_variables.csv")
+    assert_frame_equal(expected, result)
+
+
+def test_questions_from_generations_with_multiple_versions():
+    result = create_questions_from_generations(
+        "y1",
+        "tests/data/logical_variables.csv",
+        "tests/data/generations_with_multiple_versions.csv",
     )
     expected = pd.read_csv("tests/data/questions_variables.csv")
     assert_frame_equal(expected, result)
