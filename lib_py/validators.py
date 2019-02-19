868e4c6ec292ee87dfc5bf2b79eb3d91fa6e5931
afuetterer <fuetterh@posteo.de>
2019-02-19 13:30:19 +0100

    Add validators.py

---
+++ lib_py/validators.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+def test_questions_questionnaries():
+    """
+    Every question in "questions.csv" should reference a
+    questionnaire that is defined in questionnaires.csv
+    """
+    questions = pd.read_csv("metadata/questions.csv")
+    assert "questionnaire" in questions.columns
+    assert "study" in questions.columns
+
+    questionnaires = pd.read_csv("metadata/questionnaires.csv")
+    assert "questionnaire" in questionnaires.columns
+    assert "study" in questionnaires.columns
+
+    # merge two files based on "questionnaire"
+    merged = questions.merge(questionnaires, on="questionnaire", how="outer")
+
+    questionnaires_not_used = merged[merged["study_x"].isna()]
+    if len(questionnaires_not_used) != 0:
+        print(questionnaires_not_used)
+        print(questionnaires_not_used["questionnaire"].unique())
+
+    questionnaires_not_defined = merged[merged["study_y"].isna()]
+
+    if len(questionnaires_not_defined) != 0:
+        unique_questionnaires = questionnaires_not_defined["questionnaire"].unique()
+        print()
+        print(
+            f'Questionnaires not defined in "questionnaires.csv": {unique_questionnaires}'
+        )
+        print('\nAffected rows in "questions.csv"')
+        print(questions[questions["questionnaire"].isin(unique_questionnaires)])
+        print()
+
+    assert len(questionnaires_not_used) == 0
+    assert (
+        len(questionnaires_not_defined) == 0
+    ), 'Some questionnaires are not defined in "questionnaires.csv"'
