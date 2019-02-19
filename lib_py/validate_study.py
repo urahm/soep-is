b50798b45273dad7ac94615c46b24ab06bf5c6db
afuetterer <fuetterh@posteo.de>
2019-02-19 18:24:11 +0100

    Refactor validation script

--- lib_py/validate_study.py
+++ lib_py/validate_study.py
@@ -8,10 +8,12 @@ import great_expectations as ge
 ANSWERS_FILENAME = "../metadata/answers.csv"
 QUESTIONS_FILENAME = "../metadata/questions.csv"
 QUESTIONNAIRES_FILENAME = "../metadata/questionnaires.csv"
+DATASETS_FILENAME = "../metadata/logical_datasets.csv"
 
 ANSWERS_EXPECTATIONS_FILENAME = "expectations/answers.json"
 QUESTIONS_EXPECTATIONS_FILENAME = "expectations/questions.json"
 QUESTIONNAIRES_EXPECTATIONS_FILENAME = "expectations/questionnaires.json"
+DATASETS_EXPECTATIONS_FILENAME = "expectations/datasets.json"
 
 
 def load_expectations(filename):
@@ -43,34 +45,45 @@ def questionnaires():
     )
 
 
-def validate(data_filename, expectations_filename):
-    expectations_config = load_expectations(expectations_filename)
-    df = ge.read_csv(data_filename, expectations_config=expectations_config)
+@pytest.fixture
+def datasets():
+    return ge.read_csv(
+        DATASETS_FILENAME,
+        expectations_config=load_expectations(DATASETS_EXPECTATIONS_FILENAME),
+    )
+
+
+def validate(df):
     results = df.validate(result_format="COMPLETE")
     for result in results["results"]:
         if result["success"] is False:
             click.secho(json.dumps(result, indent=2), fg="red")
+            indices = result["result"]["unexpected_index_list"]
+            print(df.iloc[indices])
+
     assert results["success"] is True
 
 
-def validate_answers():
-    validate(ANSWERS_FILENAME, ANSWERS_EXPECTATIONS_FILENAME)
+def validate_answers(answers):
+    validate(answers)
 
 
 def validate_logical_variables():
     pass
 
 
-def validate_logical_datasets():
-    pass
+def validate_logical_datasets(datasets):
+    # remove datasets with conceptual_dataset "raw"
+    datasets = datasets[datasets["conceptual_dataset"] != "raw"]
+    validate(datasets)
 
 
 def validate_generations():
     pass
 
 
-def validate_questions():
-    validate(QUESTIONS_FILENAME, QUESTIONS_EXPECTATIONS_FILENAME)
+def validate_questions(questions):
+    validate(questions)
 
 
 def foreign_key_relationship(df1, df2, key):
@@ -97,5 +110,5 @@ def validate_answers_questionnaire_relationship(answers, questionnaires):
     foreign_key_relationship(answers, questionnaires, "questionnaire")
 
 
-def validate_questionnaires():
-    validate("../metadata/questionnaires.csv", "expectations/questionnaires.json")
+def validate_questionnaires(questionnaires):
+    validate(questionnaires)
